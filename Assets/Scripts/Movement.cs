de5249a23495dc64920daf7512e0ee696196d593
Kacper Kuszek <kuszekkacper@gmail.com>
2026-04-06 21:08:06 +0200

    Added left right movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] InputAction action;
-    [SerializeField] float up = 30f;
-
+    [SerializeField] InputAction upAction;
+    [SerializeField] InputAction rotation;
+    
+    [SerializeField] float up = 3000f;
+    [SerializeField] float leftRight = 10f;
     
     Rigidbody rb;
 
@@ -14,21 +16,50 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
+
     private void OnEnable()
     {
-        action.Enable();
+        upAction.Enable();
+        rotation.Enable();
     }
     private void OnDisable()
     {
-        action.Disable();
+        upAction.Disable();
+        rotation.Disable();
     }
-    
+
     private void FixedUpdate()
     {
-        if(action.IsPressed())
+        ProcessActionUp();
+        ProcessRotation();
+    }
+    private void ProcessActionUp()
+    {
+        if(upAction.IsPressed())
         {
-            rb.AddRelativeForce(0,up * Time.deltaTime,0);
+            rb.AddRelativeForce(Vector3.up * up * Time.deltaTime);
         }
     }
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if(rotation.IsPressed())
+        {
+            if(rotationInput == -1)
+            {
+                rb.AddRelativeForce(Vector3.left * leftRight * Time.deltaTime);
+            } else
+            {
+                rb.AddRelativeForce(Vector3.right * leftRight * Time.deltaTime);
+            }
+        
+        }
+
+
+    }
+
+
+
+
 
 }
